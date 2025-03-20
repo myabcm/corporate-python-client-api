0f1cb8f19f6030c93a8806b430ddfb9036bbe056
Clovis Ribeiro <clovis.ribeiro@myabcm.com>
2025-03-20 09:23:14 -0400

    Changed package version to 0.1.5 add made a quick change to the new samples

--- samples/calculate_model.py
+++ samples/calculate_model.py
@@ -5,12 +5,12 @@ import platform
 # This sample uses the myabcm_corporate_client_api package from Python Package index.
 # If you want to use the source code of the package direcly for debugging/improving it,
 # COMMENT the import below and UNCOMMENT the next 4 lines of code after the line below.
-# from myabcm_corporate_client_api import CorporateServer
+from myabcm_corporate_client_api import CorporateServer
 
-module_dir = os.path.abspath("../src/myabcm_corporate_client_api")
-if module_dir not in sys.path:
-    sys.path.append(module_dir)
-from corporate_server_module import CorporateServer
+#module_dir = os.path.abspath("../src/myabcm_corporate_client_api")
+#if module_dir not in sys.path:
+#    sys.path.append(module_dir)
+#from corporate_server_module import CorporateServer
 
 
 try:
